4f5935be1e17188cea98db7d43df7040ea44e5f7
Qori El-Hafizh <contact@qori.dev>
2022-05-24 10:00:08 +0700

    first url shortener

---
+++ pages/jsu.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from "react"
+
+export default function JudulSkripsiUdinus() {
+    useEffect(() => {
+        window.location.href = "https://qrhfz.github.io/JudulSkripsiUdinus/";
+    }, []);
+    return <></>
+}
